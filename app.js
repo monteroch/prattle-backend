bb9ed7aa1bddc887e9a57cf7e5b6b617a84c3c1a
Christian Montero Martínez <christianmontero08@gmail.com>
2020-02-16 23:59:40 -0800

    Work on sending messages

--- app.js
+++ app.js
@@ -39,8 +39,14 @@ mongoose.connect('mongodb://localhost:4444/prattle-backend', { useNewUrlParser: 
     var io = socketIO.listen(server);
     io.on('connect', (socket) => {
         socket.on('NEW_MESSAGE', (message) => {
-            console.log(`[${message.author}] ${message.date}: ${message.text}`);
-            socket.emit("MESSAGE_FROM_SERVER", {message});
+            console.log(`[${message.conversationId}]-[${message.author}] ${message.date}: ${message.text}`);
+            io.to(message.conversationId).emit('MESSAGE_FROM_SERVER', {
+                conversationId: message.conversationId,
+                author: message.author,
+                date: message.date,
+                text: message.text
+            });
+            // socket.emit("MESSAGE_FROM_SERVER", {message});
         });
 
         socket.on('LOGGED_IN', (data) => {
